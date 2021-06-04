d3810c08579226c0ee91b65cb6aa6499941752b7
fukuch <h.fukuchi@i-voice.co.jp>
2021-06-04 09:04:17 +0900

    0604

--- fukuchproduct/src/main/resources/static/js/table.js
+++ fukuchproduct/src/main/resources/static/js/table.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
-    $('#list').DataTable({
-    "language": {
-   "url": "//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Japanese.json"
-  },
-  
-   
+
+
     
-    });
+$('#list').DataTable( {
+		"language": {
+   		"url": "//cdn.datatables.net/plug-ins/3cfcc339e89/i18n/Japanese.json"
+  		},
+		
+        "dom": "lf"+"<'row'<'col-sm-6'B>>"+"trip",
+        "searching":true,
+        "paging":true,
+        "order":[[1,"desc"]],
+        "ordering":true,
+        "columnDefa":[{
+        	"targets":[3],
+        	"orderable":false
+        }]
+        
+    } );
+   
 } );
