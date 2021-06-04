d120519adc59614035a5d75d12440b9445750eb6
fukuch <h.fukuchi@i-voice.co.jp>
2021-06-04 13:28:12 +0900

    src/main/javaを追加

---
+++ fukuchproduct/src/main/java/com/voice/fukuchproduct/controller/MenuController.java
@@ -0,0 +1,13 @@
+package com.voice.fukuchproduct.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MenuController {
+
+	/*//メニュー画面への遷移
+	@GetMapping("/")
+	String index() {
+	    return "list_members";
+	}*/
+}
